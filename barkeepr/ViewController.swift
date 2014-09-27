d8d7e05a4e59713d487d87aedf0ba2d7689808c7
Marc Cyr <marcacyr@gmail.com>
2014-09-27 13:36:20 -0700

    Initial Commit

---
+++ barkeepr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barkeepr
+//
+//  Created by Marc Cyr on 9/27/14.
+//  Copyright (c) 2014 Barkeepr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
