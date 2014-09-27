21f055c1e347657c0ca43cddd9d58d8426bf4cc9
Marc Cyr <marcacyr@gmail.com>
2014-09-27 14:42:46 -0700

    uitableviewcontroller

---
+++ barkeepr/drink.swift
@@ -0,0 +1,15 @@
+//
+//  drink.swift
+//  barkeepr
+//
+//  Created by Marc Cyr on 9/27/14.
+//  Copyright (c) 2014 Barkeepr. All rights reserved.
+//
+
+import Foundation
+
+struct Drink {
+    let category : String
+    let name : String
+}
+
